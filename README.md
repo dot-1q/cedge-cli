7f93250af8bbfe00f7d9a8ad9d82f6b58750469f
dot-1q <tiagobarros99@ua.pt>
2023-03-19 15:02:25 +0000

    Added readme and formatted code

--- README.md
+++ README.md
@@ -1,1 +1,92 @@
-# Aether ROC CLI tool to help manage an Aether Deployment.
+# Aether ROC CLI tool to manage an Aether Deployment.
+
+This is a simple command line tool that I created to help me manage my Aether-In-a-Box deployment. It is still very early stages and I've only implemented 
+the API calls that I use the most.
+Since this was initially for AiaB, if you wish to use it for a standalone environment you have to change the base API URL:
+```
+roc_api_url = "http://localhost:31194/aether-roc-api/aether/v2.1.x/"
+```
+
+To add new subscribers to the Core we have to communicate with a different [API](https://docs.sd-core.opennetworking.org/master/configuration/config_rest.html).
+This requires that we grab the config service IP address that's located on the omec namespace and note the _webui_ service IP address:
+```
+kubectl get svc -n omec
+```
+
+## Requirements
+```
+pip3 install -r requirements.txt
+```
+
+## Usage
+
+As of now, these are all the commands available:
+
+```
+Usage: mngmt.py [OPTIONS] ENTERPRISE SITE COMMAND [ARGS]...
+
+  Command line tool to manage Aether's ROC.
+
+  An enterprise and site name need to be passed as all API endpoints require
+  it.
+
+Options:
+  --help  Show this message and exit.
+
+Commands:
+  add-sim              Add a subscriber to Aether's core.
+  assign-device-group  Assign a device to a device group.
+  create-slice         Create a new Slice.
+  create-upf           Create a new UPF.
+  get-site             Get a description of all the sites of an enterprise.
+  setup-sim            Add a new sim card and device associated to it.
+```
+All the commands require you to provide a enterprise and site name, as all API calls require it.
+Ex:
+```
+python3 mngmt.py ename sname setup-sim newsim newdevice 208930000000000
+```
+
+This is an example of a command which provides optional arguments. The help page for this specific command is in the Help section below.
+```
+python3 mngmt.py a a create-slice --sn "Slice 4" --sd "New Slice" --mbr_dl 1000000 --mbr_dl_bs 250000 --mbr_ul 1000000 --mbr_ul_bs 250000 slice4 dg4 040404 4 upf4
+```
+
+## Help
+
+You can bring up the definition and help documentation of any command by doing:
+
+```
+python3 mngmt.py ename sname COMMAND --help
+```
+
+Example output of command _python3 mngmt.py ename sname create-slice --help_
+```
+Usage: mngmt.py ENTERPRISE SITE create-slice [OPTIONS] SLICE_ID DEVICE_GROUP
+                                             SERVICE_DIFFERENTIATOR
+                                             SLICE_SERVICE_TYPE UPF_ID
+
+  Create a new Slice. Each Slice must be in only one site and must only have
+  one UPF. Multiple device groups  may be added to the slice, but this version
+  does not yet support this.
+
+  SLICE_ID is a unique indentifier for the slice. ex: "slice4"
+
+  DEVICE_GROUP is the device group ID associated to this slice. ex: "dg4"
+
+  SERVICE_DIFFERENTIATOR is a 6 HEX characters value for the slice. ex:
+  "040404"
+
+  SLICE_SERVICE_TYPE is SST value between 1-255. ex: "4"
+
+  UPF_ID is the UPF ID associated to this slice. ex: "upf4"
+
+Options:
+  --sn TEXT            Slice Name  [default: Slice]
+  --sd TEXT            Slice Description  [default: Network Slice]
+  --mbr_dl INTEGER     Slice Maximum Bit Rate Downlink  [default: 100000000]
+  --mbr_dl_bs INTEGER  Slice MBR Downlink Burst Size  [default: 625000]
+  --mbr_ul INTEGER     Slice Maximum Bit Rate Uplink  [default: 100000000]
+  --mbr_ul_bs INTEGER  Slice MBR Uplink Burst Size  [default: 625000]
+  --help               Show this message and exit.
+```
--- mngmt.py
+++ mngmt.py
@@ -1,24 +1,20 @@
 import requests
 import click
 
-"""
-Example of an endpoint with enterprise and site
-http://localhost:31194/aether-roc-api/aether/v2.1.x/aiab-enterprise/site/aiab-site/sim-card/
-"""
-
 # Base API URL
 # This if or Aether-in-a-Box and is meant to be run on the same VM that it is deployed, this means
 # that on an Aether Standalone deployment the API port will be different.
 roc_api_url = "http://localhost:31194/aether-roc-api/aether/v2.1.x/"
 
+
 @click.group()
 @click.argument('enterprise', nargs=1, type=click.STRING)
 @click.argument('site', nargs=1, type=click.STRING)
 @click.pass_context
-def aether_cli(ctx,enterprise,site):
+def aether_cli(ctx, enterprise, site):
     """
     Command line tool to manage Aether's ROC.
-    
+
     An enterprise and site name need to be passed as all API endpoints require it.
     """
     ctx.ensure_object(dict)
@@ -27,11 +23,12 @@ def aether_cli(ctx,enterprise,site):
     ctx.obj['ENTERPRISE'] = enterprise
     ctx.obj['SITE'] = site
 
+
 @aether_cli.command()
 @click.argument('imsi', nargs=1, type=click.INT)
 @click.argument('address', nargs=1, type=click.STRING)
-@click.option('--port',default=5000,type=click.INT,help='Subscriber config service Port',show_default=True)
-def add_sim(imsi,address,port):
+@click.option('--port', default=5000, type=click.INT, help='Subscriber config service Port', show_default=True)
+def add_sim(imsi, address, port):
     """
     Add a subscriber to Aether's core. An imsi sim card number should be provided.
 
@@ -43,31 +40,33 @@ def add_sim(imsi,address,port):
     ADDRESS is the address of the subscriber config server.
     """
     # SD-Core subscriber's provisioning api endpoint.
-    url = "http://{a}:{p}/api/subscriber/imsi-{i}".format(a=address,p=port,i=imsi)
+    url = "http://{a}:{p}/api/subscriber/imsi-{i}".format(
+        a=address, p=port, i=imsi)
     print(url)
 
     req_body = {
-        "UeId":str(imsi),
-        "plmnId":"20893",
-        "opc":"981d464c7c52eb6e5036234984ad0bcf",
-        "key":"5122250214c33e723a5dd523fc145fc0",
-        "sequenceNumber":"16f3b3f70fc2"
-        }
+        "UeId": str(imsi),
+        "plmnId": "20893",
+        "opc": "981d464c7c52eb6e5036234984ad0bcf",
+        "key": "5122250214c33e723a5dd523fc145fc0",
+        "sequenceNumber": "16f3b3f70fc2"
+    }
 
     # Send POST
-    response = requests.post(url,json=req_body)
+    response = requests.post(url, json=req_body)
     print(response)
     print(response.content)
+
 
 @aether_cli.command()
 @click.pass_context
 @click.argument('sim-id', nargs=1, type=click.STRING)
 @click.argument('device-id', nargs=1, type=click.STRING)
 @click.argument('imsi', nargs=1, type=click.STRING)
-@click.option('--sd',default="New Sim Card",type=click.STRING,help='Sim card description',show_default=True)
-@click.option('--sn',default="New UE Sim",type=click.STRING,help='Sim card name',show_default=True)
-@click.option('--dn',default="New Device",type=click.STRING, help='Device name',show_default=True)
-def setup_subscriber(ctx,sim_id,device_id,imsi,sd,sn,dn):
+@click.option('--sd', default="New Sim Card", type=click.STRING, help='Sim card description', show_default=True)
+@click.option('--sn', default="New UE Sim", type=click.STRING, help='Sim card name', show_default=True)
+@click.option('--dn', default="New Device", type=click.STRING, help='Device name', show_default=True)
+def setup_sim(ctx, sim_id, device_id, imsi, sd, sn, dn):
     """
     Add a new sim card and device associated to it.
     This can only be done to sim card numbers that have been previously added to the ROC Core.
@@ -77,6 +76,7 @@ def setup_subscriber(ctx,sim_id,device_id,imsi,sd,sn,dn):
     SIM_ID is a unique sim identifier. ex: 'sim-12345'
 
     DEVICE_ID is a unique device identifier. ex: 'device-12345'
+
     IMSI is the sim card number. ex: '208930000000000'
     """
 
@@ -84,7 +84,8 @@ def setup_subscriber(ctx,sim_id,device_id,imsi,sd,sn,dn):
     enterprise = ctx.obj['ENTERPRISE']
     site = ctx.obj['SITE']
 
-    url = roc_api_url + "{e}/site/{s}/sim-card/{sim}".format(e=enterprise,s=site,sim=sim_id)
+    url = roc_api_url + \
+        "{e}/site/{s}/sim-card/{sim}".format(e=enterprise, s=site, sim=sim_id)
 
     req_body = {
         "description": sd,
@@ -92,31 +93,34 @@ def setup_subscriber(ctx,sim_id,device_id,imsi,sd,sn,dn):
         "enable": True,
         "imsi": imsi,
         "sim-id": sim_id
-        }
+    }
 
     response = requests.post(url, json=req_body)
     print(response)
     print(response.content)
-    
+
     ###################################################################
 
-    url = roc_api_url + "{e}/site/{s}/device/{device}".format(e=enterprise,s=site,device=device_id)
+    url = roc_api_url + \
+        "{e}/site/{s}/device/{device}".format(e=enterprise,
+                                              s=site, device=device_id)
 
     req_body = {
         "device-id": device_id,
         "display-name": dn,
         "sim-card": sim_id,
-        }
+    }
 
     response = requests.post(url, json=req_body)
     print(response)
     print(response.content)
 
+
 @aether_cli.command()
 @click.pass_context
 @click.argument('device-group', nargs=1, type=click.STRING)
 @click.argument('device-id', nargs=1, type=click.STRING)
-def assign_device_group(ctx,device_group,device_id):
+def assign_device_group(ctx, device_group, device_id):
     """
     Assign a device to a device group.
 
@@ -129,27 +133,29 @@ def assign_device_group(ctx,device_group,device_id):
     enterprise = ctx.obj['ENTERPRISE']
     site = ctx.obj['SITE']
 
-    url = roc_api_url + "{e}/site/{s}/device-group/{dg}/device/{d}".format(e=enterprise,s=site,dg=device_group,d=device_id)
+    url = roc_api_url + "{e}/site/{s}/device-group/{dg}/device/{d}".format(
+        e=enterprise, s=site, dg=device_group, d=device_id)
 
     req_body = {
         "device-id": device_id,
         "enable": True
-        }
+    }
 
     # Send POST
     response = requests.post(url, json=req_body)
     print(response)
     print(response.content)
 
+
 @aether_cli.command()
 @click.pass_context
 @click.argument('upf-id', nargs=1, type=click.STRING)
 @click.argument('address', nargs=1, type=click.STRING)
 @click.argument('config_endpoint', nargs=1, type=click.STRING)
-@click.option('--un',default="UPF",type=click.STRING,help='UPF Name',show_default=True)
-@click.option('--ud',default="User Plane Function",type=click.STRING,help='UPF Description',show_default=True)
-@click.option('--up',default=8805,type=click.INT,help='UPF Port',show_default=True)
-def create_upf(ctx,upf_id,address,config_endpoint,un,ud,up):
+@click.option('--un', default="UPF", type=click.STRING, help='UPF Name', show_default=True)
+@click.option('--ud', default="User Plane Function", type=click.STRING, help='UPF Description', show_default=True)
+@click.option('--up', default=8805, type=click.INT, help='UPF Port', show_default=True)
+def create_upf(ctx, upf_id, address, config_endpoint, un, ud, up):
     """
     Create a new UPF. Each UPF can only be associated with a single site and slice.
 
@@ -164,7 +170,8 @@ def create_upf(ctx,upf_id,address,config_endpoint,un,ud,up):
     enterprise = ctx.obj['ENTERPRISE']
     site = ctx.obj['SITE']
 
-    url = roc_api_url + "{e}/site/{s}/upf/{u}".format(e=enterprise,s=site,u=upf_id)
+    url = roc_api_url + \
+        "{e}/site/{s}/upf/{u}".format(e=enterprise, s=site, u=upf_id)
 
     req_body = {
         "address": address,
@@ -173,7 +180,7 @@ def create_upf(ctx,upf_id,address,config_endpoint,un,ud,up):
         "display-name": un,
         "port": up,
         "upf-id": upf_id,
-        }
+    }
 
     # Send POST
     response = requests.post(url, json=req_body)
@@ -181,6 +188,8 @@ def create_upf(ctx,upf_id,address,config_endpoint,un,ud,up):
     print(response.content)
 
 # TODO: Add multiple device groups to a slice
+
+
 @aether_cli.command()
 @click.pass_context
 @click.argument('slice-id', nargs=1, type=click.STRING)
@@ -188,13 +197,13 @@ def create_upf(ctx,upf_id,address,config_endpoint,un,ud,up):
 @click.argument('service-differentiator', nargs=1, type=click.STRING)
 @click.argument('slice-service-type', nargs=1, type=click.STRING)
 @click.argument('upf_id', nargs=1, type=click.STRING)
-@click.option('--sn',default="Slice",type=click.STRING,help='Slice Name',show_default=True)
-@click.option('--sd',default="Network Slice",type=click.STRING,help='Slice Description',show_default=True)
-@click.option('--mbr_dl',default=100000000,type=click.INT,help='Slice Maximum Bit Rate Downlink',show_default=True)
-@click.option('--mbr_dl_bs',default=625000,type=click.INT,help='Slice MBR Downlink Burst Size',show_default=True)
-@click.option('--mbr_ul',default=100000000,type=click.INT,help='Slice Maximum Bit Rate Uplink',show_default=True)
-@click.option('--mbr_ul_bs',default=625000,type=click.INT,help='Slice MBR Uplink Burst Size',show_default=True)
-def create_slice(ctx,slice_id,device_group,service_differentiator,slice_service_type,upf_id,sn,sd,mbr_dl,mbr_dl_bs,mbr_ul,mbr_ul_bs):
+@click.option('--sn', default="Slice", type=click.STRING, help='Slice Name', show_default=True)
+@click.option('--sd', default="Network Slice", type=click.STRING, help='Slice Description', show_default=True)
+@click.option('--mbr_dl', default=100000000, type=click.INT, help='Slice Maximum Bit Rate Downlink', show_default=True)
+@click.option('--mbr_dl_bs', default=625000, type=click.INT, help='Slice MBR Downlink Burst Size', show_default=True)
+@click.option('--mbr_ul', default=100000000, type=click.INT, help='Slice Maximum Bit Rate Uplink', show_default=True)
+@click.option('--mbr_ul_bs', default=625000, type=click.INT, help='Slice MBR Uplink Burst Size', show_default=True)
+def create_slice(ctx, slice_id, device_group, service_differentiator, slice_service_type, upf_id, sn, sd, mbr_dl, mbr_dl_bs, mbr_ul, mbr_ul_bs):
     """
     Create a new Slice. Each Slice must be in only one site and must only have one UPF. Multiple device groups 
     may be added to the slice, but this version does not yet support this. 
@@ -214,36 +223,38 @@ def create_slice(ctx,slice_id,device_group,service_differentiator,slice_service_
     enterprise = ctx.obj['ENTERPRISE']
     site = ctx.obj['SITE']
 
-    url = roc_api_url + "{e}/site/{s}/slice/{sl}".format(e=enterprise,s=site,sl=slice_id)
+    url = roc_api_url + \
+        "{e}/site/{s}/slice/{sl}".format(e=enterprise, s=site, sl=slice_id)
 
     req_body = {
         "connectivity-service": "5g",
         "default-behavior": "ALLOW-ALL",
-        "description":sd,
+        "description": sd,
         "device-group": [
             {
                 "device-group": device_group,
                 "enable": True
             }
             # TODO: Multiple device groups go here
-            ],
+        ],
         "display-name": sn,
         "mbr": {
             "downlink": mbr_dl,
             "downlink-burst-size": mbr_dl_bs,
             "uplink": mbr_ul,
             "uplink-burst-size": mbr_ul_bs
-            },
+        },
         "sd": service_differentiator,
         "slice-id": slice_id,
         "sst": slice_service_type,
         "upf": upf_id,
-        }
+    }
 
     # Send POST
     response = requests.post(url, json=req_body)
     print(response)
     print(response.content)
+
 
 @aether_cli.command()
 @click.pass_context
@@ -259,6 +270,7 @@ def get_site(ctx):
     response = requests.get(url)
     print(response)
     print(response.content)
+
 
 if __name__ == '__main__':
     aether_cli()
